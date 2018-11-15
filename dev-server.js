736e2679d06155bb5e5cdf865604c6ecc4425ba3
leiyunkang7 <leiyunakng7@sina.com>
2018-11-15 21:35:18 +0800

    热模块替换

---
+++ dev-server.js
@@ -0,0 +1,18 @@
+const webpackDevServer = require('webpack-dev-server')
+const webpack = require('webpack')
+
+const config = require('./webpack.config.js')
+
+const options = {
+  contentBase: './dist',
+  hot: true,
+  host: 'localhost'
+}
+
+webpackDevServer.addDevServerEntrypoints(config, options)
+const complier = webpack(config)
+const server = new webpackDevServer(complier, options)
+
+server.listen(5000, 'localhost', () => {
+  console.log('dev server listening on port 5000')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
+const webpack = require('webpack')
+
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   output: {
     filename: '[name].bundle.js',
@@ -16,12 +17,22 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
