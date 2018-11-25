1a149c2ff2f94d2cd9175cece8446a29a085173a
yunkang lei <leiyunkang7@sina.com>
2018-11-25 19:12:06 +0800

    使用环境变量

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,10 +2,17 @@ const merge = require('webpack-merge')
 
 const common = require('./webpack.common.js')
 
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  }
-})
+module.exports = env => {
+
+  console.info('NODE_ENV', env.NODE_ENV)
+
+  console.info('Production', env.production)
+
+  return merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist'
+    }
+  })
+}
